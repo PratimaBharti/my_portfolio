edc5adcf44d0c5e5c36aeeba97e5b7ce23f184f2
pratimabharti <pratimabhartisittu@gmail.com>
2024-01-27 22:17:59 +0530

    updated

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -6,21 +6,21 @@ export const Data = [
     {
       id: 1,
       image: Image1,
-      title: "Jhon Doe",
+     // title: "Jhon Doe",
       // description:
       //   "A really good job, all aspects of the project were followed step by step and with good results.",
     },
     {
       id: 2,
       image: Image2,
-      title: "Harry Clinton",
+     // title: "Harry Clinton",
       // description:
       //   "A really good job, all aspects of the project were followed step by step and with good results.",
     },
     {
       id: 3,
       image: Image3,
-      title: "Sara Cill",
+    //  title: "Sara Cill",
       // description:
       //   "A really good job, all aspects of the project were followed step by step and with good results.",
     },
